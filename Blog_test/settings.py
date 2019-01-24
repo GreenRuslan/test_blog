7dc4782ec19c3d65d9e6b0e93bd03b61c957ee03
Ruslan <greenruslan@gmail.com>
2019-01-25 00:22:51 +0200

    Add templates and new app

--- Blog_test/settings.py
+++ Blog_test/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
+    'blog.apps.BlogConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'Blog_test.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'Blog_test/static')
+]
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
 TEMPLATE_LOADERS = ('django_jinja.loaders.AppLoader', 'django_jinja.loaders.FileSystemLoader')
